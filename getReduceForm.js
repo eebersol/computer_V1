ddecb573d6651b06a39920a542b3fdba27260f4d
Edouard EBERSOLDT <eebersol@e3r1p11.42.fr>
2017-11-10 18:22:31 +0100

    premeir degree ok, second degree in progress

--- getReduceForm.js
+++ getReduceForm.js
@@ -10,23 +10,6 @@ function reduce_x(strTab)
 	return (strTab);
 }
 
-function basicCalc(strTab)
-{
-	let calc = [];
-	let unknow = [];
-	for (var i = 0; i < strTab.length; i++)
-	{
-		if (strTab[i].search('X') == -1)
-			calc.push(strTab[i]);
-		else
-			unknow.push(strTab[i])
-	}
-	calc = eval(calc.join(''))
-	calc = calc >= 0 ? '+' + calc : '-' + calc; 
-	unknow.push(calc)
-	return (unknow)
-}
-
 function moveLeft(str)
 {
 	let toMove = str.split('=')[1];
@@ -35,106 +18,79 @@ function moveLeft(str)
 	for (var i =0; i < toMove.length; i++)
 	{
 		if (i == 0 && toMove[i][0] != '-' && toMove[i][0] != '+')
-			toMove[i] = '-' + toMove[i][0]
+			toMove[i] = '-' + toMove[i]
 		if (toMove[i][0] == '-')
 			toMove[i][0] = '+';
 		else if (toMove[i][0] == '+')
-			toMovep[i][0] = '-'
+			toMove[i][0] = '-'
 	}
 	str = str.split('=')[0] + toMove.join('');
 	return (str);
 }
 
-function sort_priority(strTab)
-{
-	let arr1 		= [];
-	let arr2 		= [];
-	let arr3 		= [];
-	let finalArray 	= [];
-
-	arr1 = strTab.filter((str) => {
-		if (str.search(/X\^/i) != -1)
-		{
-			console.log(isNaN(parseInt(str)) ? 1 : parseInt(str))
-			console.log(arr1)
-			return isNaN(parseInt(str)) ? '1' : parseInt(str)
-		}
-	})
-	arr2 = strTab.filter((str) => {
-		if (str[str.length-1] == 'X')
-		{
-			str = isNaN(parseInt(str)) == NaN ? 1 : parseInt(str)
-		}
-	})
-	arr3 = strTab.filter((str) => {
-		if (str.search('X') == -1)
-		{
-
-			str = isNaN(parseInt(str)) == NaN ? 1 : parseInt(str)
-		}
-	})
-	console.log('ic' ,arr1)
-	finalArray.push(arr1.reduce(function(pv, cv) { return pv + cv; }, 0) + 'X^2');
-	finalArray.push(arr2.reduce(function(pv, cv) { return pv + cv; }, 0) + 'X');
-	finalArray.push(arr3.reduce(function(pv, cv) { return pv + cv; }, 0));
-	console.log(finalArray)
-	return (finalArray);
-}
-
 function sort_priority_bis(strTab)
 {
-	this.value = {};
-	this.valueTab = [];
-
-	for (var i = 0; i < strTab.length; i++)
-	{
-		this.value.sign 	= strTab[i][0] == '-' ? '-' : '+';
-		this.value.number 	= isNaN(parseInt(strTab[i][0])) ? 1 : parseInt(strTab[i]);
-		this.value.power 	= strTab[i].search(/(?:X\^([0-9]|\d\d+))/) != -1 ? strTab[i].substr(strTab[i].search(/(?:X\^([0-9]|\d\d+))/), strTab[i].length) : null;
-		this.valueTab.push(this.value);
-	}
-}
-
-function get_polynomial(strTab)
-{
-	let flag;
-
 	for (var i=0; i < strTab.length; i++)
 	{
-		if (strTab[i].search(/X\^/) != -1)
-			flag = flag > strTab[i][strTab[i].search(/X\^/)+2] ? flag : strTab[i][strTab[i].search(/X\^/)+2];
+		console.log(strTab[i])
 	}
-	console.log('Polynomial degree: ', flag);
+	this.valueTab = [];
+	for (var i = 0; i < strTab.length; i++)
+	{
+		let len = strTab[i].length -1;
+		let val = strTab[i]
+		this.value = {};
+		this.value.sign 	= val[0] == '-' ? '-' : '+';
+		this.value.number 	= isNaN(parseInt(strTab[i])) ? 1 : parseInt(val);
+		this.value.power 	= val[len-1] == '^' ? parseInt(val[len]) : val[len] == 'X' ? 1 : null
+		this.valueTab.push(this.value);
+	}
+	for (var i = 1; i < this.valueTab.length;i++)
+	{
+		let len  = this.valueTab.lemgth - 1;
+		let cell = this.valueTab[i];
+		let cellBefore = this.valueTab[i-1];
+		if (this.valueTab[i-1].power < this.valueTab[i].power)
+		{
+			let tmp = cellBefore;
+			this.valueTab[i-1] = cell;
+			this.valueTab[i] = tmp;
+			i = 0;
+		}
+	}
+	this.valueTab.sort( (objA, objB) => {return (objA > objB)})
+	let tmpTab = [];
+	for (var i = 0;i < this.valueTab.length; i++)
+	{
+		if (this.valueTab[i+1] && this.valueTab[i].power == this.valueTab[i+1].power &&  !this.valueTab[i+1].done)
+		{
+			this.valueTab[i+1].done = 1;
+			this.valueTab[i+1].number = this.valueTab[i+1].number > 0 && this.valueTab[i+1].sign == '-' ? -parseInt(this.valueTab[i+1].number) : this.valueTab[i+1].number;
+			this.valueTab[i].number = this.valueTab[i].number > 0 && this.valueTab[i].sign == '-' ? -parseInt(this.valueTab[i].number) : this.valueTab[i].number;
+			
+			let tmp 	= {};
+			tmp.sign 	= this.valueTab[i].number + this.valueTab[i+1].number >= 0 ? '+' : '-';
+			tmp.number 	= this.valueTab[i].number + this.valueTab[i+1].number;
+			tmp.power	= this.valueTab[i].power;
+			tmpTab.push(tmp)
+		}
+		else if (!this.valueTab[i].done)
+			tmpTab.push(this.valueTab[i]);
+	}
+	this.valueTab = tmpTab;
+	for (var i = 0; i < this.valueTab.length; i++)
+	{
+		console.log(this.valueTab[i]);
+	}
 }
 
-function check_solve(strTab)
+function get_polynomial()
 {
-	let flag;
+	this.polynomial = 0;
 
-	flag = 0;
-	console.log('Reduced form : ' + strTab.join(' '));
-	get_polynomial(strTab);
-	for(var i=0; i < strTab.length; i++)
-	{
-		if (strTab[i].search(/(?:X\^([3-9]|\d\d+))/g) != -1)
-			flag++;
-	}
-	flag == 0 ? 0 : console.log('The polynomial degree is stricly greater than 2, I can\'t solve.')
-	return (flag);
-}
-
-function get_a_b_c(str, char)
-{
-	this.equation[char] = '';
-	for (var i=0; i < str.length; i++)
-	{
-		if ((str[i] == '+' || str[i] == '-') &&  str[i=1] == 'X')
-			this.equation[char] = '1';
-		else if (!str[i] || str[i] == 'X')
-			break;
-		else if (str[i] != '+')
-			this.equation[char] += str[i];
-	}
+	for (var i=0; i < this.valueTab.length; i++)
+		this.polynomial = this.valueTab[i].power > this.polynomial && this.valueTab[i].number != 0 ? this.valueTab[i].power : this.polynomial; 
+	console.log('Polynomial degree: ', this.polynomial);
 }
 
 function ft_sqrt(n, g) {
@@ -151,19 +107,24 @@ function ft_sqrt(n, g) {
 
 function calc_delta()
 {
- 	this.equation.delta = (this.equation.b * this.equation.b)  - (4 * this.equation.a * this.equation.c)
+	if (this.valueTab.length != 3)
+		console.log('Error not reduced form');
+
+	this.a = this.valueTab[0]
+	this.b = this.valueTab[1]
+	this.c = this.valueTab[2]
+
+ 	this.delta = (this.b.number * this.b.number)  - (4 * this.a.number * this.c.number)
 }
 
 function delta_pos()
 {
-	let a 		= parseFloat(this.equation.a)
-	let b		= parseFloat(this.equation.b)
-	let delta 	= parseFloat(this.equation.delta)
-
-	console.log('a : ', a, '\nb : ', b, '\nDelta :', ft_sqrt(delta))
-	this.result = (-b - ft_sqrt(delta)) / (2 * a)
+	console.log('Calcul delta positif')
+	console.log((-this.b.number- ft_sqrt(this.delta)))
+	console.log((2 * this.a.number))
+	this.result = (-this.b.number- ft_sqrt(this.delta)) / (2 * this.a.number)
 	console.log(this.result.toFixed(6));
-	this.result = (-b + ft_sqrt(delta)) / (2 * a)
+	this.result = (-this.b.number + ft_sqrt(this.delta)) / (2 * this.a.number)
 	console.log(this.result.toFixed(6));
 }
 
@@ -174,7 +135,6 @@ function delta_neg()
 
 function delta_zero()
 {
-	console.log('ici')
 	let a 		= parseFloat(this.equation.a)
 	let b		= parseFloat(this.equation.b)
 	console.log(a, b)
@@ -182,6 +142,24 @@ function delta_zero()
 	console.log(this.result);
 }
 
+function move_right()
+{
+	this.result = '';
+	for (var i = this.valueTab.length-1; i > 0; i--)
+	{
+		if (this.valueTab[i].sign == '+' && this.valueTab[i].power == null)
+			this.valueTab[i].sign = '-';
+		else if (this.valueTab[i].sign == '-')
+			this.valueTab[i].sign = '+';
+		else if (this.valueTab[i].sign == '/')
+			this.valueTab[i].sign = '*';
+		else if ((this.valueTab[i].sign == '*') || this.valueTab[i].power == 1)
+			this.valueTab[i].sign = '/'
+		else
+			console.log('toto')
+		this.result += this.valueTab[i].sign + this.valueTab[i].number;
+	}
+}
 function getReduceForm()
 {
 	this.basicSign 			= ['+', '-'];
@@ -189,16 +167,21 @@ function getReduceForm()
 	this.equation.complet 	= process.argv[2];
 	this.equation.complet 	= this.equation.complet.split(' ');
 	this.equation.complet 	= this.equation.complet.join('');
-	this.equation.complet 	= reduce_x(moveLeft(this.equation.complet).match(/([+-]?)(?:([^+x-]+)?(?:x(?:\^([\d\/]+))?)|([^+x-]+))/g));
+	this.equation.complet = moveLeft(this.equation.complet).match(/([+-]?)(?:([^+x-]+)?(?:x(?:\^([\d\/]+))?)|([^+x-]+))/g)
+	console.log('After all move : ', this.equation.complet)
+	this.equation.complet 	= reduce_x(this.equation.complet);
 	this.equation.complet 	= sort_priority_bis(this.equation.complet);
-	this.equation.complet 	= basicCalc(this.equation.complet);
-	if (check_solve(this.equation.complet) != 0)
-		return ;
-	get_a_b_c(this.equation.complet[0], 'a');
-	get_a_b_c(this.equation.complet[1], 'b');
-	get_a_b_c(this.equation.complet[2], 'c');
+	get_polynomial(this.equation.complet)
 	calc_delta();
-	this.equation.delta > 0 ? delta_pos() : this.equation.delta  < 0 ? delta_neg() : delta_zero();
+	if (this.polynomial > 2)
+			return console.log('The polynomial degree is stricly greater than 2, I can\'t solve.');
+	else if (this.polynomial == 2)
+		this.delta > 0 ? delta_pos() : this.equation.delta  < 0 ? delta_neg() : delta_zero();
+	else if (this.polynomial == 1)
+	{
+		move_right();
+		console.log('The solution is: \n', eval(this.result));
+	}
 }
 
 getReduceForm();
